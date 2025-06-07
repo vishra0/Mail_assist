e4f808acaf0980a6dea66119fac87569c3dd4043
Vishnu Rao L <vraolalige@gmail.com>
2025-06-08 00:00:48 +0530

    Initial commit

---
+++ src/test/java/com/email/assist/EmailAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.assist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
